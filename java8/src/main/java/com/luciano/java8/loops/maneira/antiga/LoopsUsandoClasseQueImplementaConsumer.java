017516a24e3498969b78ee5675fd89786183d8f2
luc-git-projects <luc.git.projects@hotmail.com>
2024-06-22 22:05:53 -0300

    removendo classes que nao estavam sendo usadas no exemplo do foreach usando lambda

--- java8/src/main/java/com/luciano/java8/loops/maneira/antiga/LoopsUsandoClasseQueImplementaConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.luciano.java8.loops.maneira.antiga;
-
-public class LoopsUsandoClasseQueImplementaConsumer {
-
-    public static void main(String[] args) {
-        
-    }
-
-}
--- java8/src/main/java/com/luciano/java8/loops/maneira/nova/SubclassMostrador.java
+++
@@ -1,5 +0,0 @@
-package com.luciano.java8.loops.maneira.nova;
-
-public class SubclassMostrador extends Mostrador{
-
-}
